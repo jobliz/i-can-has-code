5c25c9e45c8f169340571e405d2e081310ceccb3
José <cloudfaust@gmail.com>
2012-04-02 17:55:59 -0430

    Commented Python Euler 10

--- python/eulers/problem-010.py
+++ python/eulers/problem-010.py
@@ -1,4 +1,8 @@
-# http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# erath is http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# mod 2, 3, 5 elimination and square upper limit from
+# http://en.wikipedia.org/wiki/Sieve_of_Atkin
+# TODO: Atkin's sieve
+
 def erath(limit):
     check = lambda n: n % 2 != 0 and n % 3 != 0 and n % 5 != 0
     sieve = [n for n in xrange(1, limit) if check(n)][1:]
